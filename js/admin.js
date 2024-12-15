03a7e55f8024e01a3580520971c59eb11ef30eda
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-15 13:10:47 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function() {
   const response = await fetch("/admin");
   const data = await response.json();
-  console.log(JSON.stringify(data));
+  console.log(data);
 });
