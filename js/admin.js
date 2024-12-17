01210e04389d77706ba75d7efc7ee8f65dc6c9a8
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-16 21:44:23 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', async function() {
   const response = await fetch("/personal");
   const data = await response.json();
-  console.log(data[0].sharof);
+  const obj = employees.map(item => {
+    let totalHours = item.workDays.reduce((acc, hoursDay) => acc + hoursDay.totalHours, 0);
+    let totalSalary = item.workDays.reduce((acc, hoursDay) => acc + hoursDay.totalDailySalary, 0);
+    return { name: item.name, position: item.position, salary: item.salaryPerHour, totalHours: totalHours,  totalSalary: totalSalary };
+  });
+
+  console.log(obj);
 });
